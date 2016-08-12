9da9df266cf4b7eb615ea99ff3d506df2b7dfee2
Zachary Foster <zacharyfoster1989@gmail.com>
2016-08-12 06:14:36 -0700

    working on dtabase comparison

--- settings.R
+++ settings.R
@@ -22,7 +22,7 @@ max_taxonomy_depth <- 4
 min_seq_count <- NULL
 just_bacteria <- TRUE
 max_mismatch <- 10 # percentage mismatch tolerated in pcr
-pcr_success_cutoff <- 0.80 # Used to subset for graphing
+pcr_success_cutoff <- 0.95 # Used to subset for graphing
 min_seq_length <- 1200 # Use to encourage full length sequences
 forward_primer = c("515F" = "GTGYCAGCMGCCGCGGTAA")
 reverse_primer = c("806R" = "GGACTACNVGGGTWTCTAAT")
