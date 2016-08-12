f24f2b3f9e72cbd7e8e6b4d4e00d27333af2dbf0
fosterz <zacharyfoster1989@gmail.com>
2016-08-11 17:53:19 -0700

    moved settings; modified subsetting

--- settings.R
+++ settings.R
@@ -1,20 +1,29 @@
-# This file stores parameters shared by multiple Rmd files in this directory.
+## ---- rendering_settings ----
+# display
+options(width = 100)
+set.seed(1)
+# Knitr
+library(knitr)
+library(grid)
+opts_chunk$set(cache = TRUE, dev = 'png', fig.width = 7, fig.height = 7, warning = FALSE, message = FALSE, 
+               dpi = 300, cache.lazy = FALSE, autodep = TRUE)
+# Citations
+library(knitcitations)
+cite_options(max.names = 2)
 
-# shared settings
+## ---- io_settings ----
 input_folder <- "raw_input"
 output_folder <- "results"
 output_format <- ".pdf"
 
-# display
-options(width = 100)
-set.seed(1)
 
-# Knitr settings
-library(knitr)
-library(grid)
-opts_chunk$set(cache = TRUE, dev='png', fig.width = 7, fig.height = 7, warning = FALSE, message = FALSE, 
-               dpi = 300, cache.lazy = FALSE, autodep = TRUE)
-
-# Citation settings
-library(knitcitations)
-cite_options(max.names = 2)
+## ---- database_comparison_settings ----
+max_taxonomy_depth <- 4
+min_seq_count <- NULL
+just_bacteria <- TRUE
+max_mismatch <- 10 # percentage mismatch tolerated in pcr
+pcr_success_cutoff <- 0.80 # Used to subset for graphing
+min_seq_length <- 1200 # Use to encourage full length sequences
+forward_primer = c("515F" = "GTGYCAGCMGCCGCGGTAA")
+reverse_primer = c("806R" = "GGACTACNVGGGTWTCTAAT")
+pcr_success_color_scale = c("red", "orange", "yellow", "green", "cyan")
