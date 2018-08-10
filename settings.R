57a3e0e4d757d5557f9b73df3c1323a09cb34c42
zachary-foster <zacharyfoster1989@gmail.com>
2018-08-10 01:17:45 -0700

    started integration with workshop

--- settings.R
+++ settings.R
@@ -1,14 +1,52 @@
+
 ## ---- rendering_settings ----
-# display
-options(width = 80)
-set.seed(1)
-# Knitr
-library(knitr)
-library(grid)
-opts_chunk$set(dev = 'png', fig.width = 7, fig.height = 7, warning = FALSE, message = FALSE, dpi = 300)
-# Citations
-library(knitcitations)
-cite_options(max.names = 2)
+# Knitr settings
+knitr::opts_chunk$set(echo = TRUE, warning = FALSE, autodep = TRUE,
+                      dev = 'png', fig.width = 10, fig.height = 10)
+knitr::opts_chunk$set(fig.align = "center")
+options(crayon.enabled = TRUE, tibble.max_extra_cols = 10)
+
+# Citation options (might not be used)
+library("knitcitations")
+cite_options(citation_format = "pandoc", max.names = 3, style = "html", hyperlink = "to.doc")
+bib <- read.bibtex("bibliography.bibtex")
+
+# Knitr hooks
+knitr::knit_hooks$set(
+  hide_button = function(before, options, envir) {
+    if (is.character(options$hide_button)) {
+      button_text = options$hide_button
+    } else {
+      button_text = "Show solution"
+    }
+    block_label <- paste0("hide_button", options$label)
+    if (before) {
+      return(paste0(sep = "\n",
+                    '<button class="btn btn-danger" data-toggle="collapse" data-target="#', block_label, '"> ', button_text, ' </button>\n',
+                    '<div id="', block_label, '" class="collapse">\n'))
+    } else {
+      return("</div><br />\n")
+    }
+  },
+  output = function(x, options){
+    x <- gsub(x, pattern = "<", replacement = "&lt;")
+    x <- gsub(x, pattern = ">", replacement = "&gt;")
+    paste0(
+      "<pre class=\"r-output\"><code>",
+      fansi::sgr_to_html(x = x, warn = TRUE, term.cap = "256"),
+      # ansistrings::ansi_to_html(text = x, fullpage = FALSE),
+      "</code></pre>"
+    )
+  }
+)
+
+# Display settings
+options(width = 100)
+
+# Glossary settings
+library(glossary)
+gloss <- glossary(definitions_path = "_glossary_definitions.Rmd", glossary_path = "appendix--00--glossary.Rmd")
+
 
 ## ---- io_settings ----
 input_folder <- "raw_input" # Where all the large input files are. Ignored by git. 
